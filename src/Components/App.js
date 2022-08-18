4e0376630118b81069a60ea7498d6f1a8d4623c3
Anthoniselvi <anthoniselvi33@gmail.com>
2022-08-18 11:40:50 +0530

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchInput from './SearchInput';
+
+
+const App = () => {
+return (
+    <div className='ui container' style={{marginTop:'40px'}}>
+    <SearchInput />
+    </div> 
+)
+}
+
+
+
+
+export default App;
